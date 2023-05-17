32c102f33222e372be5437d573958cb11f84260b
KaneWatson <webitgenius@gmail.com>
2023-05-17 07:25:33 +0200

    fixed webhooks and booking bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -81,7 +81,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = catchAsync(async (session) => {
   try {
-    console.log(session);
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email }))._id;
     const price = session.amount_total / 100;
@@ -92,7 +91,7 @@ const createBookingCheckout = catchAsync(async (session) => {
       price,
     });
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
 });
 
